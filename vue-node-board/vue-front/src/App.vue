498c6779521cb2e16412fe0ec616dafe53747fc0
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2024-03-29 16:40:56 +0900

    [F] FEAT: Vuetify3 기본 적용

---
+++ vue-node-board/vue-front/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-btn color="primary">Click Me</v-btn>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: anitialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
