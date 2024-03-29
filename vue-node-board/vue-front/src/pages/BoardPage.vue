517b1bc36866ab73922928c7c847b014422a1ec6
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2024-03-29 17:49:42 +0900

    [F] FEAT: 메인페이지

--- vue-node-board/vue-front/src/pages/BoardPage.vue
+++ vue-node-board/vue-front/src/pages/BoardPage.vue
@@ -1,5 +1,11 @@
 <template>
   <v-app>
-    <div></div>
+    <v-app-bar app color="red" dark>
+      <v-spacer></v-spacer>
+      <v-app-bar-title>
+        <div :style="{textAlign: 'center', fontSize:'xx-large'}">게시판</div>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
   </v-app>
 </template>
--- vue-node-board/vue-front/src/pages/MainPage.vue
+++ vue-node-board/vue-front/src/pages/MainPage.vue
@@ -1,5 +1,46 @@
 <template>
   <v-app>
-    <div></div>
+    <v-app-bar app color="red" dark>
+      <v-spacer></v-spacer>
+      <v-app-bar-title>
+        <div :style="{textAlign: 'center', fontSize:'xx-large'}">메인화면</div>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-row align="center" :style="{justifyContent: 'center'}">
+          <v-col cols="12" md="2" align="center">
+            <v-btn color="light-green" @click="movetoboard1"
+                   :style="{height:'50px', width:'170px', fontWeight:'bold', fontSize:'large'}">게시판1</v-btn>
+          </v-col>
+          <v-col cols="12" md="2" align="center">
+            <v-btn color="yellow" @click="movetoboard2"
+                   :style="{height:'50px', width:'170px', fontWeight:'bold', fontSize:'large'}">게시판2</v-btn>
+          </v-col>
+          <v-col cols="12" md="2" align="center">
+            <v-btn color="orange" type="submit" @click="movetoboard3"
+                   :style="{height:'50px', width:'170px', fontWeight:'bold', fontSize:'large'}">게시판3</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+  methods: {
+    // params로 게시판 구분, query로 페이지 구분
+    movetoboard1() {
+      window.location.href = '/board/1/?page=1'
+    },
+    movetoboard2() {
+      window.location.href = '/board/2/?page=1'
+    },
+    movetoboard3() {
+      window.location.href = '/board/3/?page=1'
+    }
+  }
+}
+</script>
